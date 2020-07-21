0e5d605485068d7ef9daf1393f1bef0cefa1fb1f
Jianni Hu <65553010+HujiAnni@users.noreply.github.com>
2020-07-22 07:46:46 +0800

    Get events name(Berkeley)

---
+++ get events name.py
@@ -0,0 +1,8 @@
+import requests
+from bs4 import BeautifulSoup
+res=requests.get('https://events.berkeley.edu/?view=summary&timeframe=day&date=2020-07-15&tab=all_events')
+html_berkeley=res.text
+soup=BeautifulSoup(html_berkeley,'html.parser')
+events_name=soup.find_all(class_='event-title')
+for item in events_name:
+    print(item.text)
